cbfbbc20195b004344dcc21edc82c4d7af83e2fc
avad <avad@itu.dk>
2026-08-27 17:59:51 +0200

    Fucked around and made it shourter

--- w1/Perket.py
+++ w1/Perket.py
@@ -1,22 +1,16 @@
 n = int(input())
-Ss = []
-Bs = []
+W = []
 for _ in range(n):
-    s,b = map(int, input().split())
-    Ss.append(s)
-    Bs.append(b)
-
-min_val = abs(Ss[0] - Bs[0])
-
+    W.append(tuple(map(int, input().split())))
+min_val = abs(W[0][0] - W[0][1])
 
 for k in range(1,1<<n):
-    s = 1
-    b = 0
+    w = (1,0)
     for i in range(n):
         if (k&(1<<i)):
-            s *= Ss[i]
-            b += Bs[i]
-    if (min_val > abs(s-b)):
-       min_val = abs(s-b)
+            w = (w[0] * W[i][0],w[1] + W[i][1])
+    t = abs(w[0]-w[1])
+    if (min_val > t):
+       min_val = t
 
 print(min_val)
